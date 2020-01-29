f5ddfced211380457896809cb22946d828945973
Alexander Josef <alexander.josef@netcetera.com>
2020-01-29 20:56:56 +0100

    Adding new strategy (no impl)

---
+++ src/hbm-legacy-naming.strategy.ts
@@ -0,0 +1,72 @@
+import { DefaultNamingStrategy, NamingStrategyInterface } from 'typeorm';
+import { snakeCase } from 'typeorm/util/StringUtils';
+
+// implements the NamingStrategy with a hibernate legacy naming strategy
+// this strategy is called 'legacy-hbm' as property when configuring the hibernate session factory
+// the implicit hibernate naming strategy translates camelcase property names to all lower case on the DB
+// tested with a database schema on postgreSQL from a legacy Java program that has been migrated to typeORM
+//
+// todo: implement strategy below from copied code:
+//
+export class HbmLegacyNamingStrategy extends DefaultNamingStrategy
+  implements NamingStrategyInterface {
+
+  tableName(className: string, customName: string): string {
+    return customName ? customName : snakeCase(className);
+  }
+
+  columnName(
+    propertyName: string,
+    customName: string,
+    embeddedPrefixes: string[],
+  ): string {
+    return (
+      snakeCase(embeddedPrefixes.join('_')) +
+      (customName ? customName : snakeCase(propertyName))
+    );
+  }
+
+  relationName(propertyName: string): string {
+    return snakeCase(propertyName);
+  }
+
+  joinColumnName(relationName: string, referencedColumnName: string): string {
+    return snakeCase(relationName + '_' + referencedColumnName);
+  }
+
+  joinTableName(
+    firstTableName: string,
+    secondTableName: string,
+    firstPropertyName: string,
+    secondPropertyName: string,
+  ): string {
+    return snakeCase(
+      firstTableName +
+        '_' +
+        firstPropertyName.replace(/\./gi, '_') +
+        '_' +
+        secondTableName,
+    );
+  }
+
+  joinTableColumnName(
+    tableName: string,
+    propertyName: string,
+    columnName?: string,
+  ): string {
+    return snakeCase(
+      tableName + '_' + (columnName ? columnName : propertyName),
+    );
+  }
+
+  classTableInheritanceParentColumnName(
+    parentTableName: any,
+    parentTableIdPropertyName: any,
+  ): string {
+    return snakeCase(parentTableName + '_' + parentTableIdPropertyName);
+  }
+
+  eagerJoinRelationAlias(alias: string, propertyPath: string): string {
+    return alias + '__' + propertyPath.replace('.', '_');
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,2 @@
 export { SnakeNamingStrategy } from './snake-naming.strategy';
+export { HbmLegacyNamingStrategy } from './hbm-legacy-naming.strategy';
